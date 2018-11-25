feac6ff5b3f6e490503d1fbba497bac27a2b6683
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-11-25 21:31:47 +0100

    Added update functionality via Hibernate

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+T
+$src/main/resources/hibernate.cfg.xml,3\1\3176feae82bf4ff4f68f72c9d6d3b850a3110c22
+S
+#src/main/java/dal/entities/Car.java,4\d\4d9f2d9bcceb728f6fe3d8486df1c2d74dee063c
+Q
+!src/main/java/test/UpdateCar.java,5\4\54cd664df4dd6fcc1e7a28dcf0bdf160fcc4c2d3
+Z
+*src/main/java/test/RetrieveCreatedCar.java,e\e\ee2910b03841d93f68caca3dd734d5e7e5a253e5
+V
+&src/main/java/test/dataConnection.java,c\3\c3fa042e086917e3e4ed8e9d00279bea461821be
+O
+src/main/java/test/GetCars.java,0\8\08573f072379f812462618ca532d323280ef9271
+Q
+!src/main/java/test/DeleteCar.java,7\c\7c7d1896eaa95ed58b1e7979c71bd4c257b7c951
+^
+.src/main/java/dal/HibernateSessionFactory.java,b\5\b5674c6f61fd3124daca1038d8e513089d6d07d1
---
+++ src/main/java/test/UpdateCar.java
@@ -0,0 +1,57 @@
+package test;
+
+import dal.entities.Car;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+public class UpdateCar {
+
+    public static void main(String[] args){
+
+        // Create new session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Car.class)
+                .buildSessionFactory();
+
+        // Create session
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            // Use the session object to save Java object to database
+            int carId = 1;
+
+            // Start a transaction
+            session.beginTransaction();
+
+            // Get a car with correct id
+            Car car = session.get(Car.class, carId);
+
+            // Update create data
+            Random random = new Random();
+            car.setPrice((random.nextInt(50000) * random.nextInt(10)));
+            car.setMileage((random.nextInt(10000) * random.nextInt(10)));
+
+            // Commit the transaction
+            session.getTransaction().commit();
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            session.createQuery("UPDATE dal.entities.Car SET test_date = '2018-11-25'")
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+
+            session.close();
+        }
+        finally {
+            sessionFactory.close();
+        }
+    }
+}
