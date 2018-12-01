2bbc98454c7c9cdcb24ef373b411953ff6e80842
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-12-01 17:32:50 +0100

    Trying to fix Travis-CI

--- src/main/java/autoscoutbackend/AutoScoutApplication.java
+++ src/main/java/autoscoutbackend/AutoScoutApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AutoScoutApplication {
@@ -23,5 +29,13 @@ public class AutoScoutApplication {
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000");
             }
         };
+    }
+
+    @RequestMapping("/car")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String getTimeMessage(){
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 }
